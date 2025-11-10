9be8b266b95525d50c75f6cc1767b930989cd2ce
José Arzuaga <2196891+JoseInTheArena@users.noreply.github.com>
2025-11-10 22:43:21 +0000

    New practice problem added

---
+++ src/ones.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ * Write a function:
+
+ * int solution(int N);
+
+ * that, given an integer N, returns the number of times the digit 1 occurs in decimal representations of all positive integers not exceeding N.
+
+ * For example, given N = 13 the function should return 6, because:
+
+ * - all the positive integers that do not exceed 13 are 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 and 13;
+ * - digit 1 occurs six times altogether: once in number 1, once in number 10, twice in number 11, once in number 12 and once in number 13.
+ * - Write an efficient algorithm for the following assumptions:
+
+ * N is an integer within the range [0..100,000,000].
+ * 
+ * 
+ * 
+ */
+
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N) {
+    let count = 0;
+    for (let i = 1; i <= N; i++) {
+        // Increment count by the number of '1's in the string representation of i
+        count += i.toString().split('').filter(x => x === '1').length;
+    }
+    return count; 
+}
+
+
+let [, , N] = process.argv;
+console.log(solution(parseInt(N, 10)));
